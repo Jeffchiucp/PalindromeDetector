c7f71ff2826cdb79514c34c5882afd6f5d5123d1
JeffChiu <jchiu@kcc.local>
2016-03-16 10:47:10 -0700

    Initial Commit

---
+++ PalindromeDetector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PalindromeDetector
+//
+//  Created by JeffChiu on 3/16/16.
+//  Copyright © 2016 JeffChiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PalindromeDetector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PalindromeDetector
+//
+//  Created by JeffChiu on 3/16/16.
+//  Copyright © 2016 JeffChiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
