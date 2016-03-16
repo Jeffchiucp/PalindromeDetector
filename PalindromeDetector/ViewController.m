48f1e21947eb8ef780c16b0663718f20de1a205c
JeffChiu <jchiu@KCC.local>
2016-03-16 14:04:26 -0700

    first commit

--- PalindromeDetector/ViewController.m
+++ PalindromeDetector/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UILabel *yesNoLabel;
+@property (weak, nonatomic) IBOutlet UIButton *makePalindrome;
+
+@property (weak, nonatomic) IBOutlet UILabel *outPutLabel;
+
 
 @end
 
@@ -16,12 +22,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+}
+- (IBAction)checkPalindrome:(UIButton *)sender
+{
+    NSString *palindrome = self.textField.text;
+    NSUInteger length = palindrome.length;
+    NSLog(@"Length equals: %lu", length);
+    
+    for (int i=0; i < length; i++)
+    {
+        NSLog(@"[palindrome characterAtIndex:%i] = %c", i, [palindrome characterAtIndex:i]);
+        if ([palindrome characterAtIndex:i] != [palindrome characterAtIndex:length-1-i])
+        {
+            NSLog(@"nope");
+            self.yesNoLabel.text = @"nope";
+            return;
+        }
+        
+    }
+    
+    self.yesNoLabel.text =@"yes";
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)createPalindrome:(UIButton *)sender
+{
+    NSString *firstPart = self.textField.text;
+    NSMutableString *secondPart = [NSMutableString new];
+    
+    NSUInteger length = firstPart.length;
+    for (int i=0; i < length; i++)
+    {
+        unichar c = [firstPart characterAtIndex:length -1-i];
+        [secondPart appendFormat: @"%c", c];
+    }
+    // stringByAppendingFormat:@"%C", c];
+    self.outPutLabel.text = [NSString stringWithFormat:@"%@%@", firstPart, secondPart];
 }
+
 
 @end
